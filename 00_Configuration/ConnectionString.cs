9f5f62beacb1206ce2fa032eea3abe3e76dd8cbe
Tom Van den Rul <tom.vandenrul@sesvanderhave.com>
2021-03-24 08:19:09 +0100

    Uitbreiding oefeningen'

--- 00_Configuration/ConnectionString.cs
+++ 00_Configuration/ConnectionString.cs
@@ -1,13 +1,11 @@
-﻿using System;
-
-namespace _00_Configuration
+﻿namespace _00_Configuration
 {
     public static class ConnectionString
     {
         // Boekendatabase files zitten mee in deze repository
-        public static string Boekendatabase = "Data Source=.\\SQLEXPRESS01;Initial Catalog=Boekendatabase;Integrated Security=True;Trusted_Connection=True";
+        public static string Boekendatabase = "Data Source=.\\SQLEXPRESS;Initial Catalog=Boekendatabase;Integrated Security=True;Trusted_Connection=True";
         // AdventureWorks is een sample database van Microsoft
         // https://docs.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver15&tabs=ssms
-        public static string AdventureWorks = "Data Source=.\\SQLEXPRESS01;Initial Catalog=AdventureWorks;Integrated Security=True;Trusted_Connection=True";
+        public static string AdventureWorks = "Data Source=.\\SQLEXPRESS;Initial Catalog=AdventureWorks;Integrated Security=True;Trusted_Connection=True";
     }
 }
--- 04_EFCore_Basis/Program.cs
+++ 04_EFCore_Basis/Program.cs
@@ -2,11 +2,19 @@
 
 namespace _05_EFCore_Boekendatabase
 {
+    // Oefening: 
+    // TIP: zie project 05_EFCore_Boekendatabase voor basis van EF core
+    // TIP: gebruik Microsoft EF Core documentatie
+    // - Uitbreiden van klasse 'PersonAddress' adhv. database properties
+    // - Uitbreiden van klasse 'Person' adhv. database properties
+    // - Aanmaken van DbContext voor AdventureWorks
+    // - Probeer de eerste 100 personen op te laden, samen met hun adressen. Toon deze in een lijstje -- Een persoon kan meerdere adressen hebben
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
